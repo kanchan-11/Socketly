a71283389cd699ad2ec1c758dc434e2858f65e6f
Kanchan Agarwal <agarwallkanchan29@gmail.com>
2026-03-05 22:45:07 +0530

    Added all infra and docker files with updated configs

--- backend/src/main/java/com/codinglearn/chat/backend/config/WebsocketConfig.java
+++ backend/src/main/java/com/codinglearn/chat/backend/config/WebsocketConfig.java
@@ -18,6 +18,6 @@ public void configureMessageBroker(org.springframework.messaging.simp.config.Mes
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:5173").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
     }
 }
